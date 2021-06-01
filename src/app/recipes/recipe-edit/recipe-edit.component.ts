4cd55a8dc751a27ede9fe885aed3b214e791d521
rahul <rkc.98@outlook.com>
2021-06-01 14:00:53 +0530

    Added RF to recipe edit component

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {  FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,15 +11,47 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
 id:number;
 editmode=false
-  constructor(private route:ActivatedRoute) { }
+recipeform:FormsModule;
+  constructor(private route:ActivatedRoute,private recipeservice:RecipeService) { }
 
   ngOnInit(): void {
 this.route.params.subscribe(
   (params:Params)=>{
     this.id=+params['id'];
     this.editmode=params['id']!=null;
+    this.initForm()
   }
 )
+
   }
+  onsubmit(){
+    console.log(this.recipeform);
+  }
+
+private initForm(){
+let recipename='';
+let imagePath='';
+let description='';
+if(this.editmode){
+  const recipe=this.recipeservice.getRecipe(this.id);
+  console.log("recipe",recipe);
+  
+  recipename=recipe.name;
+  imagePath=recipe.imagePath;
+  description=recipe.description
+  
+}
+
+
+  
+
+this.recipeform=new FormGroup({
+  'recipename': new FormControl(recipename,Validators.required),
+   'imagePath':new FormControl(imagePath,Validators.required),
+   'description':new FormControl(description,Validators.required)
+
+})
+
+}
 
 }
